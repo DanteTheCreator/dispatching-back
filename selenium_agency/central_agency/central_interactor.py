71b4a533fd8037121c17c3c25c9acbe3b8577061
root <root@rothschildrentals.pro>
2025-05-09 13:30:14 -0400

    full

--- selenium_agency/central_agency/central_interactor.py
+++ selenium_agency/central_agency/central_interactor.py
@@ -41,13 +41,6 @@ class CentralInteractor:
         self.current_page = 0
         self.__db_Session = db_session
         self.__in_between_delay = 1  # Adding the missing attribute with a default value
-        self.states = [
-            'AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'DC', 'FL',
-            'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME',
-            'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH',
-            'NJ', 'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI',
-            'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI',
-            'WY']
         self.__array_deduplicator = ArrayDeduplicator()
 
     def set_token(self):
